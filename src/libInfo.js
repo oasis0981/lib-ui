6eaf174d0e8367a5a85a4274beda2b34dbc4fb87
ys <hootingowl09@naver.com>
2023-06-14 17:42:23 +0900

    Design: 메인페이지 정보 수정

--- src/libInfo.js
+++ src/libInfo.js
@@ -1,10 +1,10 @@
 function setMainData(libInfo) {
   const elem = `
-    <h2>도서관 정보</h2>
-        <p>대출한 총 권수: <span id="total-loans">${libInfo.totalBorrowedBooks}권</span></p>
-        <p>대출 이용 회원 수: <span id="total-users">${libInfo.borrowUsers}명</span></p>
-        <p>미반납 총 권수: <span id="total-overdue">${libInfo.overdueBooks}권</span></p>
-        <p>반납 총 권수: <span id="total-returns">${libInfo.returnBooks}권</span></p>
+    <h2>오늘의 도서관 현황</h2>
+        <p>대출한 총 권수: <span>${libInfo.totalBorrowedBooks}권</span></p>
+        <p>대출 이용 회원 수: <span>${libInfo.borrowUsers}명</span></p>
+        <p>미반납 총 권수: <span>${libInfo.overdueBooks}권</span></p>
+        <p>반납 총 권수: <span>${libInfo.returnBooks}권</span></p>
     `;
 
   document.getElementById("libInfoData").innerHTML = elem;
