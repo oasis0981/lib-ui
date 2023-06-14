7529da9f6217b66fe6542a945ef6d91c40efc865
ys <hootingowl09@naver.com>
2023-06-14 09:48:27 +0900

    no message

--- src/libInfo.js
+++ src/libInfo.js
@@ -1,10 +1,10 @@
 function setMainData(libInfo) {
   const elem = `
     <h2>도서관 정보</h2>
-        <p>대출한 총 권수: <span id="total-loans">${libInfo.totalBorrowedBooks}</span></p>
-        <p>대출 이용 회원 수: <span id="total-users">${libInfo.borrowUsers}</span></p>
-        <p>미반납 총 권수: <span id="total-overdue">${libInfo.overdueBooks}</span></p>
-        <p>반납 총 권수: <span id="total-returns">${libInfo.returnBooks}</span></p>
+        <p>대출한 총 권수: <span id="total-loans">${libInfo.totalBorrowedBooks}권</span></p>
+        <p>대출 이용 회원 수: <span id="total-users">${libInfo.borrowUsers}명</span></p>
+        <p>미반납 총 권수: <span id="total-overdue">${libInfo.overdueBooks}권</span></p>
+        <p>반납 총 권수: <span id="total-returns">${libInfo.returnBooks}권</span></p>
     `;
 
   document.getElementById("libInfoData").innerHTML = elem;
